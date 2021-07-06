8d71dc6c045997cbcad04b0a312ab9c708a0604c
Vitório Eugenio Settin Wingert <vitoriowingert@hotmail.com>
2021-07-06 16:30:28 -0300

    Initial code

---
+++ letmeask/src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function Button() {
+  const [counter, setCounter] = useState(0);
+
+  function increment() {
+    // counter += 1;
+    setCounter(counter + 1);
+    console.log(counter);
+  }
+
+  return (
+    <button onClick={increment}>
+      {counter}
+    </button>
+  )
+}
+
+export { Button };
